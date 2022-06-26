2370cf117029beaf335f693797f3e111ee204d49
david19060015 <david.it@optiktunggal.com>
2022-06-26 21:45:15 +0700

    desktop version

---
+++ data.json.js
@@ -0,0 +1,378 @@
+const data = [{
+        "adult": false,
+        "backdrop_path": "/4bEL2GEAe5QLzGNT109hLMBgIrM.jpg",
+        "genre_ids": [
+            35,
+            10751,
+            16
+        ],
+        "id": 973608,
+        "original_language": "ko",
+        "original_title": "Larva Pendant",
+        "overview": "This silly short-film sequel to \"Larva Island\" and \"The Larva Island Movie\" follows farting friends Red and Yellow's bumbling misadventures in the city.",
+        "popularity": 686.839,
+        "poster_path": "/nPW5AkzoQSO8dWhE8WZMFvC41iW.jpg",
+        "release_date": "2022-05-25",
+        "title": "Larva Pendant",
+        "video": false,
+        "vote_average": 7.4,
+        "vote_count": 41
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/hXTWVJMsI9BkxMLliqL1j0FT55t.jpg",
+        "genre_ids": [
+            28
+        ],
+        "id": 606402,
+        "original_language": "ko",
+        "original_title": "야차",
+        "overview": "Nicknamed after a human-devouring spirit, the ruthless leader of an overseas black ops team takes up a dangerous mission in a city riddled with spies.",
+        "popularity": 415.097,
+        "poster_path": "/whZ8OkI7W8cBOmtoiFyb72Ekt3U.jpg",
+        "release_date": "2022-04-08",
+        "title": "Yaksha: Ruthless Operations",
+        "video": false,
+        "vote_average": 6.2,
+        "vote_count": 94
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/okUGREWC4LN2TSjFX0AOiNJhzdN.jpg",
+        "genre_ids": [
+            99
+        ],
+        "id": 965143,
+        "original_language": "ko",
+        "original_title": "사이버 지옥: n번방을 무너뜨려라",
+        "overview": "Anonymous and exploitative, a network of online chat rooms ran rampant with sex crimes. The hunt to take down its operators required guts and tenacity.",
+        "popularity": 274.523,
+        "poster_path": "/eRlW6yvXHyXPuN0Ea6u6Sc48lGm.jpg",
+        "release_date": "2022-05-17",
+        "title": "Cyber Hell: Exposing an Internet Horror",
+        "video": false,
+        "vote_average": 7,
+        "vote_count": 40
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/wgdWNBqPIAH29ZsL3I0AVx7XRed.jpg",
+        "genre_ids": [
+            28,
+            9648,
+            53
+        ],
+        "id": 615173,
+        "original_language": "ko",
+        "original_title": "마녀(魔女) Part2. The Other One",
+        "overview": "A girl wakes up in a huge secret laboratory. then accidentally meets another girl who is trying to protect her house from a gang. The mystery girl overthrows the gang with her unexpected powers, and laboratory staff set out to find her.",
+        "popularity": 208.564,
+        "poster_path": "/A3FZJyML1vITrGdaICHJPMD658V.jpg",
+        "release_date": "2022-06-15",
+        "title": "The Witch: Part 2. The Other One",
+        "video": false,
+        "vote_average": 6.3,
+        "vote_count": 3
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/tuXjQ2zoSGZmDK9LJQeQze4qt0b.jpg",
+        "genre_ids": [
+            18
+        ],
+        "id": 783127,
+        "original_language": "ko",
+        "original_title": "불도저에 탄 소녀",
+        "overview": "Haeyoung is a hopeless girl. She lives with her father who runs a Chinese restaurant, and a younger sibling. One day, her father has a mysterious accident, which places her in a position of responsibility. She is now not only the head of the house and must look after her younger sibling, but also an investigator tasked with finding the truth about the accident, and a fighter standing against the ugly world.",
+        "popularity": 111.727,
+        "poster_path": "/8YNpBRuAMIp5Z7YrRdxyugNZ8vU.jpg",
+        "release_date": "2022-04-07",
+        "title": "The Girl on a Bulldozer",
+        "video": false,
+        "vote_average": 7.8,
+        "vote_count": 5
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/pwpw1veVNW2Sls5sGbA9mqMYN49.jpg",
+        "genre_ids": [
+            28,
+            12,
+            35,
+            36
+        ],
+        "id": 591120,
+        "original_language": "ko",
+        "original_title": "해적: 도깨비 깃발",
+        "overview": "A gutsy crew of Joseon pirates and bandits battle stormy waters, puzzling clues and militant rivals in search of royal gold lost at sea.",
+        "popularity": 105.435,
+        "poster_path": "/xtZd4rfcOeVXs2kYEyzkt2VSPLI.jpg",
+        "release_date": "2022-01-26",
+        "title": "The Pirates: The Last Royal Treasure",
+        "video": false,
+        "vote_average": 6.7,
+        "vote_count": 101
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/iuM006FCrVXqvwLg4R0hkST0HPy.jpg",
+        "genre_ids": [
+            80,
+            28
+        ],
+        "id": 619803,
+        "original_language": "ko",
+        "original_title": "범죄도시 2",
+        "overview": "Followed by Ma Seok-do, who heads to a foreign country to extradite a suspect. However, he discovers additional murder cases and learns about a killer who had committed crimes against tourists for many years.",
+        "popularity": 57.922,
+        "poster_path": "/v6y1WI9XR9vnhhLhvFfS11mlmIA.jpg",
+        "release_date": "2022-05-18",
+        "title": "The Roundup",
+        "video": false,
+        "vote_average": 6.8,
+        "vote_count": 15
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/xByXRGWUzCHdiENxBH2urvsVAFm.jpg",
+        "genre_ids": [
+            10749,
+            35
+        ],
+        "id": 760517,
+        "original_language": "ko",
+        "original_title": "모럴센스",
+        "overview": "Love never hurt so good for two co-workers who enter a contractual relationship as partners in consensual play, pleasure and pain.",
+        "popularity": 68.184,
+        "poster_path": "/yQgGYiHUoDYeA4TbYlghpA5lmKH.jpg",
+        "release_date": "2022-02-11",
+        "title": "Love and Leashes",
+        "video": false,
+        "vote_average": 6.7,
+        "vote_count": 134
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/dom8lzl2iU46nDu6s4lBolBNjQs.jpg",
+        "genre_ids": [
+            18
+        ],
+        "id": 736732,
+        "original_language": "ko",
+        "original_title": "브로커",
+        "overview": "A group of people, brought together by a baby box, set off on a journey that will lead to destinations they never expected.",
+        "popularity": 36.775,
+        "poster_path": "/ulGCgWnidH9LJTRL9rMV23cLtQb.jpg",
+        "release_date": "2022-06-08",
+        "title": "Broker",
+        "video": false,
+        "vote_average": 7.2,
+        "vote_count": 7
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/A1bWhTFQKkhF1yhSKWosSyzn2Hp.jpg",
+        "genre_ids": [
+            80,
+            18,
+            10749,
+            9648
+        ],
+        "id": 705996,
+        "original_language": "ko",
+        "original_title": "헤어질 결심",
+        "overview": "Hae-Joon, a seasoned detective, investigates the suspicious death of a man on a mountaintop. Soon, he begins to suspect Seo-rae, the deceased's wife, while being unsettled by his attraction to her.",
+        "popularity": 21.375,
+        "poster_path": "/vvdEFKmp9q5F4V3PhCj9FWUFPC2.jpg",
+        "release_date": "2022-06-29",
+        "title": "Decision to Leave",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/u8VBaEESHAAc5tDWFc8IjXHw1eC.jpg",
+        "genre_ids": [
+            35,
+            18,
+            10749
+        ],
+        "id": 936621,
+        "original_language": "ko",
+        "original_title": "물오징어6969",
+        "overview": "Three married women had always been dissatisfied sexually with their husbands, and Hyoji, who recently divorced due to her husband's erectile dysfunction. They gathered after a long time and had a drink complaining to each other, and they received a business card from an unidentified handsome man, saying, \"Don't you want to play tteok pounding game?\" The four of them head to the meeting place and meet suspicious agents who were waiting for them.",
+        "popularity": 16.085,
+        "poster_path": "/dgSR6qBiQSqaa9QfSMiODAEOF3O.jpg",
+        "release_date": "2022-01-27",
+        "title": "Sex Game 6969",
+        "video": false,
+        "vote_average": 2,
+        "vote_count": 1
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/dRKWAdh2zrXFDSK17IuS4V30Mo3.jpg",
+        "genre_ids": [
+            18
+        ],
+        "id": 982834,
+        "original_language": "ko",
+        "original_title": "The Boxer",
+        "overview": "",
+        "popularity": 9.953,
+        "poster_path": "/qKvCCMIvexxNnZRcFi5Eb0vM10S.jpg",
+        "release_date": "2022-06-23",
+        "title": "The Boxer",
+        "video": false,
+        "vote_average": 5,
+        "vote_count": 1
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/lbZzieieeSFRNU8sa1COMyOlpRI.jpg",
+        "genre_ids": [
+            99
+        ],
+        "id": 744965,
+        "original_language": "ko",
+        "original_title": "니얼굴",
+        "overview": "Eunhye draws caricatures for living at the Munhori Free Market. Though Eunhye has Down’s Syndrome, the passion of the artist in “Your Face” is relentless. She has described more than two thousand faces in caricature with her own gaze. Your Face is a film that observes her life as she does of other people’s faces, following the daily life of Eunhye, an enthusiastic artist who is sometimes quirky or full of joyfulness. Director Seo Dongil who reflected his own life in The Little Girl, The Big Girl, and the Man in between, portraits Eunhye’s adulthood and her family in a respectful manner.",
+        "popularity": 11.04,
+        "poster_path": "/faWX1XIiuZ5xyYJXaMUoNWR4A00.jpg",
+        "release_date": "2022-06-23",
+        "title": "Your Face",
+        "video": false,
+        "vote_average": 7,
+        "vote_count": 1
+    },
+    {
+        "adult": false,
+        "backdrop_path": null,
+        "genre_ids": [
+            10749,
+            35
+        ],
+        "id": 871821,
+        "original_language": "ko",
+        "original_title": "Meongmongi",
+        "overview": "What would you do if you have to let the one and only family member go?  Minsu decides to marry the love of his life but the moment he proposes, the bride to be reveals the truth that has been hidden from him. She is allergic to dog saliva and took medicines when she had to meet Minsu’s little brother Runie, the golden retriever. The happiest moment suddenly turns out to be the one that needs a life-changing decision.",
+        "popularity": 10.284,
+        "poster_path": null,
+        "release_date": "2022-06-23",
+        "title": "My Lovely Puppy",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+    },
+    {
+        "adult": false,
+        "backdrop_path": null,
+        "genre_ids": [
+            10402
+        ],
+        "id": 987053,
+        "original_language": "ko",
+        "original_title": "두개의 눈",
+        "overview": "",
+        "popularity": 9.506,
+        "poster_path": "/v897flqaS9r8U8MthjtDADGFyvy.jpg",
+        "release_date": "2022-06-23",
+        "title": "두개의 눈",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+    },
+    {
+        "adult": false,
+        "backdrop_path": null,
+        "genre_ids": [
+            18
+        ],
+        "id": 980491,
+        "original_language": "ko",
+        "original_title": "우스운게 딱! 좋아!",
+        "overview": "",
+        "popularity": 8.741,
+        "poster_path": "/pFVNo8chKdSeSvBluJknlPIM43s.jpg",
+        "release_date": "2022-06-23",
+        "title": "우스운게 딱! 좋아!",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+    },
+    {
+        "adult": false,
+        "backdrop_path": null,
+        "genre_ids": [
+            16
+        ],
+        "id": 977826,
+        "original_language": "ko",
+        "original_title": "극장판 윌벤져스: 수상한 캠핑 대소동",
+        "overview": "",
+        "popularity": 8.988,
+        "poster_path": "/aqFbi9o4RgM449AeZlzIQYH1INa.jpg",
+        "release_date": "2022-06-23",
+        "title": "극장판 윌벤져스: 수상한 캠핑 대소동",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/5wjEdLx346uFexIhNfjPhpsRKAz.jpg",
+        "genre_ids": [
+            18,
+            10749
+        ],
+        "id": 948635,
+        "original_language": "ko",
+        "original_title": "딸래미들",
+        "overview": "A film about a novelist's sexual behavior to write a new article.",
+        "popularity": 10.127,
+        "poster_path": "/3z0qyd4LupeXkpoFN9d2X2w798c.jpg",
+        "release_date": "2022-02-25",
+        "title": "Little Daughters",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/hwG6UD5yDoEOyl4mSCyTmbdyuhd.jpg",
+        "genre_ids": [
+            10749
+        ],
+        "id": 743814,
+        "original_language": "ko",
+        "original_title": "인민을 위해 복무하라",
+        "overview": "Mu-kwang volunteers to be a kitchen police of Sa-taek, the divisional commander, dreaming of success. One day, Mu-kwang meets Su-ryeon by chance, who is a young and beautiful wife of the commander. Soon, he feels a strong attraction by her.",
+        "popularity": 6.978,
+        "poster_path": "/1V308VhbYx8rr4ubNE28JAgEaQt.jpg",
+        "release_date": "2022-02-23",
+        "title": "Serve the People",
+        "video": false,
+        "vote_average": 6.5,
+        "vote_count": 12
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/pAISUx3tmaAg5x4n5pLPAgEdhp6.jpg",
+        "genre_ids": [
+            28,
+            80
+        ],
+        "id": 606403,
+        "original_language": "ko",
+        "original_title": "특송",
+        "overview": "Eun-ha, who is a normal junkyard employee, secretly works as a delivery clerk that deals with unusual delivery requests. One day, Eun-ha heads to Seoul to pick up a client who is involved in a gambling crime that wants to flee overseas. However, Eun-ha meets the client's young son at the pick-up point, instead of the client himself. Kyeong-pil, a current police officer who is actually masterminding the whole gambling crime, chases after the missing child who has the security key to the bank account that holds 30 million dollars.",
+        "popularity": 12.9,
+        "poster_path": "/6v1N2SBhaF8lTBgsjfTAgVmS5q9.jpg",
+        "release_date": "2022-01-12",
+        "title": "Special Delivery",
+        "video": false,
+        "vote_average": 6.6,
+        "vote_count": 21
+    }
+];
